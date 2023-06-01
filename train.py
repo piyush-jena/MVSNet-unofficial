92f35ae50b1e46c189bed800299f88ab7af3e49a
Piyush Jena <piyushjena1996@gmail.com>
2023-06-01 15:27:58 -0400

    Testing why training is not working

--- train.py
+++ train.py
@@ -1,193 +1,234 @@
 import argparse
 import os
-
 import torch
+import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
 import time
 from torch.utils.tensorboard import SummaryWriter
 from models.MVSNet import MVSNet, mvsnet_loss
 from utils import *
+import sys
 import datetime
 
 cudnn.benchmark = True
-torch.cuda.empty_cache()
-
-###########################################################
-######################## Arguments ########################
-###########################################################
 
 parser = argparse.ArgumentParser(description='A PyTorch Implementation of MVSNet')
 parser.add_argument('--mode', default='train', help='train or test', choices=['train', 'test', 'profile'])
 parser.add_argument('--model', default='mvsnet', help='select model')
 
-parser.add_argument('--trainpath', type=str, default="../../Datasets/dtu_training/mvs_training/dtu/", help='train datapath')
-parser.add_argument('--testpath', default=None, help='test datapath')
-parser.add_argument('--trainlist', type=str, default="lists/dtu/train.txt", help='train list')
-parser.add_argument('--testlist', type=str, default="lists/dtu/test.txt", help='test list')
+parser.add_argument('--dataset', default='dtu_yao', help='select dataset')
+parser.add_argument('--trainpath', help='train datapath')
+parser.add_argument('--testpath', help='test datapath')
+parser.add_argument('--trainlist', help='train list')
+parser.add_argument('--testlist', help='test list')
 
-parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train')
+parser.add_argument('--epochs', type=int, default=16, help='number of epochs to train')
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 parser.add_argument('--lrepochs', type=str, default="10,12,14:2", help='epoch ids to downscale lr and the downscale rate')
 parser.add_argument('--wd', type=float, default=0.0, help='weight decay')
 
-parser.add_argument('--batch_size', type=int, default=1, help='train batch size')
-parser.add_argument('--numdepth', type=int, default=128, help='the number of depth values')
+parser.add_argument('--batch_size', type=int, default=12, help='train batch size')
+parser.add_argument('--numdepth', type=int, default=192, help='the number of depth values')
 parser.add_argument('--interval_scale', type=float, default=1.06, help='the number of depth values')
 
 parser.add_argument('--loadckpt', default=None, help='load a specific checkpoint')
-parser.add_argument('--logdir', default='./checkpoints/d52623', help='the directory to save checkpoints/logs')
-parser.add_argument('--resume', action='store_false', help='continue to train the model')
+parser.add_argument('--logdir', default='./checkpoints/debug', help='the directory to save checkpoints/logs')
+parser.add_argument('--resume', action='store_true', help='continue to train the model')
 
 parser.add_argument('--summary_freq', type=int, default=20, help='print and summary frequency')
 parser.add_argument('--save_freq', type=int, default=1, help='save checkpoint frequency')
 parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed')
 
-args = parser.parse_args()
-
-mode = args.mode
-loadckpt = args.loadckpt
-resume = True #args.resume
-batch_size = args.batch_size
-interval_scale = args.interval_scale
-lr = args.lr
-lr_epochs = args.lrepochs
-save_freq = args.save_freq
-summary_freq = args.summary_freq
-trainpath = args.trainpath
-testpath = None
-trainlist = args.trainlist
-testlist = args.testlist
-numdepth = args.numdepth
-logdir = args.logdir
-seed = args.seed
-weight_decay = args.wd
-epochs = args.epochs
-###########################################################
-######################## Arguments ########################
-###########################################################
-
-start_epoch = 0
-
 # parse arguments and check
-if resume:
-    assert mode == "train"
-    assert loadckpt is None
-if testpath is None:
-    testpath = trainpath
+args = parser.parse_args()
+if args.resume:
+    assert args.mode == "train"
+    assert args.loadckpt is None
+if args.testpath is None:
+    args.testpath = args.trainpath
 
-def train(trainDataset, model, optimizer, criterion, logger, start_epoch):
-    trainImgLoader = DataLoader(trainDataset, batch_size, shuffle=True, num_workers=1, drop_last=True)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
 
-    milestones = [int(epoch_idx) for epoch_idx in lr_epochs.split(':')[0].split(',')]
-    lr_gamma = 1 / float(lr_epochs.split(':')[1])
+# create logger for mode "train" and "testall"
+if args.mode == "train":
+    if not os.path.isdir(args.logdir):
+        os.mkdir(args.logdir)
+
+    current_time_str = str(datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
+    print("current time", current_time_str)
+
+    print("creating new summary file")
+    logger = SummaryWriter(args.logdir)
+
+print("Total Arguments:", sys.argv[1:])
+print_args(args)
+
+# dataset, dataloader
+trainDataset = MVSDataset(args.trainpath, args.trainlist, "train", 3, args.numdepth, args.interval_scale)
+testDataset = MVSDataset(args.testpath, args.testlist, "test", 5, args.numdepth, args.interval_scale)
+trainImgLoader = DataLoader(trainDataset, args.batch_size, shuffle=True, num_workers=8, drop_last=True)
+testImgLoader = DataLoader(testDataset, args.batch_size, shuffle=False, num_workers=4, drop_last=False)
+
+# model, optimizer
+model = MVSNet(refine=False)
+model.to(torch.device('cuda:0'))
+model_loss = mvsnet_loss
+optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999), weight_decay=args.wd)
+
+# load parameters
+start_epoch = 0
+if (args.mode == "train" and args.resume) or (args.mode == "test" and not args.loadckpt):
+    saved_models = [fn for fn in os.listdir(args.logdir) if fn.endswith(".ckpt")]
+    saved_models = sorted(saved_models, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+
+    # use the latest checkpoint file
+    loadckpt = os.path.join(args.logdir, saved_models[-1])
+
+    print("loading checkpoint ", loadckpt)
+    state_dict = torch.load(loadckpt)
+    model.load_state_dict(state_dict['model'])
+    optimizer.load_state_dict(state_dict['optimizer'])
+    start_epoch = state_dict['epoch'] + 1
+elif args.loadckpt:
+    # load checkpoint file specified by args.loadckpt
+    # no need to load optimizer and epoch because we are invoking in test mode
+    print("loading checkpoint {}".format(args.loadckpt))
+    state_dict = torch.load(args.loadckpt)
+    model.load_state_dict(state_dict['model'])
+
+print("start at epoch {}".format(start_epoch))
+print('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
+
+# main function
+def train():
+    milestones = [int(epoch_idx) for epoch_idx in args.lrepochs.split(':')[0].split(',')]
+    lr_gamma = 1 / float(args.lrepochs.split(':')[1])
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=lr_gamma, last_epoch=start_epoch - 1)
 
-    for epoch in range(start_epoch, epochs):
-        print('Epoch {}:'.format(epoch))
-
-        for batch, sample in enumerate(trainImgLoader):
-            start_time = time.time()
-
-            model.train()
-            optimizer.zero_grad()
-
-            sample = tocuda(sample)
-
-            depth_gt = sample["depth"]
-            mask = sample["mask"]
-
-            outputs = model(sample["imgs"], sample["proj_matrices"], sample["depth_values"])
-            depth_est = outputs["depth"]
-
-            loss = criterion(depth_est, depth_gt, mask)
-            loss.backward()
-            optimizer.step()
-
-            print(
-                'Epoch {}/{}, Iter {}/{}, train loss = {:.3f}, time = {:.3f}'.format(epoch, epochs, batch,
-                                                                                    len(trainImgLoader), tensor2float(loss),
-                                                                                    time.time() - start_time))
-
+    for epoch_idx in range(start_epoch, args.epochs):
+        print('Epoch {}:'.format(epoch_idx))
         lr_scheduler.step()
-        
-        save_checkpoint(model, optimizer, epoch, logdir)
+        global_step = len(trainImgLoader) * epoch_idx
 
-@make_nograd_func
-def test(testDataset, model, optimizer, criterion, logger, start_epoch):
-    testImgLoader = DataLoader(testDataset, batch_size, shuffle=False, num_workers=1, drop_last=False)
+        # training
+        for batch_idx, sample in enumerate(trainImgLoader):
+            start_time = time.time()
+            global_step = len(trainImgLoader) * epoch_idx + batch_idx
+            do_summary = global_step % args.summary_freq == 0
+            loss, scalar_outputs, image_outputs = train_sample(sample, detailed_summary=do_summary)
+            if do_summary:
+                save_scalars(logger, 'train', scalar_outputs, global_step)
+                save_images(logger, 'train', image_outputs, global_step)
+            del scalar_outputs, image_outputs
+            print(
+                'Epoch {}/{}, Iter {}/{}, train loss = {:.3f}, time = {:.3f}'.format(epoch_idx, args.epochs, batch_idx,
+                                                                                     len(trainImgLoader), loss,
+                                                                                     time.time() - start_time))
+
+        # checkpoint
+        if (epoch_idx + 1) % args.save_freq == 0:
+            torch.save({
+                'epoch': epoch_idx,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict()},
+                "{}/model_{:0>6}.ckpt".format(args.logdir, epoch_idx))
+
+        # testing
+        avg_test_scalars = DictAverageMeter()
+        for batch_idx, sample in enumerate(testImgLoader):
+            start_time = time.time()
+            global_step = len(trainImgLoader) * epoch_idx + batch_idx
+            do_summary = global_step % args.summary_freq == 0
+            loss, scalar_outputs, image_outputs = test_sample(sample, detailed_summary=do_summary)
+            if do_summary:
+                save_scalars(logger, 'test', scalar_outputs, global_step)
+                save_images(logger, 'test', image_outputs, global_step)
+            avg_test_scalars.update(scalar_outputs)
+            del scalar_outputs, image_outputs
+            print('Epoch {}/{}, Iter {}/{}, test loss = {:.3f}, time = {:3f}'.format(epoch_idx, args.epochs, batch_idx,
+                                                                                     len(testImgLoader), loss,
+                                                                                     time.time() - start_time))
+        save_scalars(logger, 'fulltest', avg_test_scalars.mean(), global_step)
+        print("avg_test_scalars:", avg_test_scalars.mean())
+
+def test():
     avg_test_scalars = DictAverageMeter()
-
-    for batch, sample in enumerate(testImgLoader):
+    for batch_idx, sample in enumerate(testImgLoader):
         start_time = time.time()
+        loss, scalar_outputs, image_outputs = test_sample(sample, detailed_summary=True)
+        avg_test_scalars.update(scalar_outputs)
+        del scalar_outputs, image_outputs
+        print('Iter {}/{}, test loss = {:.3f}, time = {:3f}'.format(batch_idx, len(testImgLoader), loss,
+                                                                    time.time() - start_time))
+        if batch_idx % 100 == 0:
+            print("Iter {}/{}, test results = {}".format(batch_idx, len(testImgLoader), avg_test_scalars.mean()))
+    print("final", avg_test_scalars)
 
-        model.eval()
+def train_sample(sample, detailed_summary=False):
+    model.train()
+    optimizer.zero_grad()
 
-        sample = tocuda(sample)
+    sample_cuda = tocuda(sample)
+    depth_gt = sample_cuda["depth"]
+    mask = sample_cuda["mask"]
 
-        depth_gt = sample["depth"]
-        mask = sample["mask"]
+    outputs = model(sample_cuda["imgs"], sample_cuda["proj_matrices"], sample_cuda["depth_values"])
+    depth_est = outputs["depth"]
 
-        outputs = model(sample["imgs"], sample["proj_matrices"], sample["depth_values"])
-        depth_est = outputs["depth"]
+    loss = model_loss(depth_est, depth_gt, mask)
+    loss.backward()
+    optimizer.step()
 
-        loss = criterion(depth_est, depth_gt, mask)
-
-        scalar_outputs = {"loss": loss}
-        image_outputs = {"depth_est": depth_est * mask, "depth_gt": sample["depth"],
-                        "ref_img": sample["imgs"][:, 0],
-                        "mask": sample["mask"]}
+    scalar_outputs = {"loss": loss}
+    image_outputs = {"depth_est": depth_est * mask, "depth_gt": sample["depth"],
+                     "ref_img": sample["imgs"][:, 0],
+                     "mask": sample["mask"]}
+    if detailed_summary:
         image_outputs["errormap"] = (depth_est - depth_gt).abs() * mask
-
         scalar_outputs["abs_depth_error"] = AbsDepthError_metrics(depth_est, depth_gt, mask > 0.5)
         scalar_outputs["thres2mm_error"] = Thres_metrics(depth_est, depth_gt, mask > 0.5, 2)
         scalar_outputs["thres4mm_error"] = Thres_metrics(depth_est, depth_gt, mask > 0.5, 4)
         scalar_outputs["thres8mm_error"] = Thres_metrics(depth_est, depth_gt, mask > 0.5, 8)
 
-        avg_test_scalars.update(scalar_outputs)
-        del scalar_outputs, image_outputs
-        print('Iter {}/{}, test loss = {:.3f}, time = {:3f}'.format(batch, len(testImgLoader), loss,
-                                                                    time.time() - start_time))
-        if batch % 100 == 0:
-            print("Iter {}/{}, test results = {}".format(batch, len(testImgLoader), avg_test_scalars.mean()))
-    
-    print("final", avg_test_scalars)
+    return tensor2float(loss), tensor2float(scalar_outputs), image_outputs
 
 
+@make_nograd_func
+def test_sample(sample, detailed_summary=True):
+    model.eval()
 
-def main():
-    if not os.path.isdir(logdir):
-        os.mkdir(logdir)
+    sample_cuda = tocuda(sample)
+    depth_gt = sample_cuda["depth"]
+    mask = sample_cuda["mask"]
 
-    print("Launch Time: ", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    outputs = model(sample_cuda["imgs"], sample_cuda["proj_matrices"], sample_cuda["depth_values"])
+    depth_est = outputs["depth"]
 
-    # dataset, dataloader
-    trainDataset = MVSDataset(trainpath, trainlist, "train", 5, numdepth, interval_scale)
-    testDataset = MVSDataset(testpath, testlist, "test", 3, numdepth, interval_scale)
+    loss = model_loss(depth_est, depth_gt, mask)
 
-    model = MVSNet(refine=False).to('cuda')
+    scalar_outputs = {"loss": loss}
+    image_outputs = {"depth_est": depth_est * mask, "depth_gt": sample["depth"],
+                     "ref_img": sample["imgs"][:, 0],
+                     "mask": sample["mask"]}
+    if detailed_summary:
+        image_outputs["errormap"] = (depth_est - depth_gt).abs() * mask
 
-    criterion = mvsnet_loss
-    optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=weight_decay)
+    scalar_outputs["abs_depth_error"] = AbsDepthError_metrics(depth_est, depth_gt, mask > 0.5)
+    scalar_outputs["thres2mm_error"] = Thres_metrics(depth_est, depth_gt, mask > 0.5, 2)
+    scalar_outputs["thres4mm_error"] = Thres_metrics(depth_est, depth_gt, mask > 0.5, 4)
+    scalar_outputs["thres8mm_error"] = Thres_metrics(depth_est, depth_gt, mask > 0.5, 8)
 
-    logger = SummaryWriter(logdir)
-
-    if mode == 'train':
-        if resume == True:
-            model, optimizer, start_epoch = load_checkpoint(model, optimizer, logdir)
-
-        train(trainDataset, model, optimizer, criterion, logger, start_epoch)
-
-    if mode == 'test':
-        if loadckpt == None:
-            model, optimizer, start_epoch = load_checkpoint(model, optimizer, logdir)
-        else:
-            model, optimizer, start_epoch = load_checkpoint(model, optimizer, logdir, loadckpt)
-
-        test(testDataset, model, optimizer, criterion, logger, start_epoch)
+    return tensor2float(loss), tensor2float(scalar_outputs), image_outputs
 
 if __name__ == '__main__':
-    main()
+    if args.mode == "train":
+        train()
+    elif args.mode == "test":
+        test()
